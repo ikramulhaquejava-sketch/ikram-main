31c6642453c6f21187f17f32d9bc2940edd6a8be
ikramulhaquejava-sketch <ikramulhaque.java@gmail.com>
2026-03-03 20:50:33 +0530

    Create anti-inspect.js

---
+++ anti-inspect.js
@@ -0,0 +1,2 @@
+// Minified anti-inspect script
+(function(){document.addEventListener('contextmenu',function(e){e.preventDefault();});document.addEventListener('keydown',function(e){if(e.keyCode==123||(e.ctrlKey&&e.shiftKey&&e.keyCode==73)||(e.ctrlKey&&e.shiftKey&&e.keyCode==67)||(e.ctrlKey&&e.keyCode==85)){e.preventDefault();}});})();
